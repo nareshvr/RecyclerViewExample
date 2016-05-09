aa21a2c9f81e963a18a42bc028d88a780591d75e
Sundeep <sundeep.penkay@duceretech.com>
2016-05-09 19:35:30 +0530

    Session table to save sessions

---
+++ app/src/main/java/ducere/lechal/pod/podsdata/Session.java
@@ -0,0 +1,137 @@
+package ducere.lechal.pod.podsdata;
+
+/**
+ * Created by sunde on 09-05-2016.
+ */
+public class Session {
+    int id;
+    String name;
+    int activityType;
+    int goalType;
+    int goalValue;
+    int milestoneFreq;
+    int steps;
+    int calories;
+    int time;
+    int distance;
+    int status;
+    boolean sync;
+    long startTime;
+    long endTime;
+
+    public Session(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getActivityType() {
+        return activityType;
+    }
+
+    public int getGoalType() {
+        return goalType;
+    }
+
+    public int getGoalValue() {
+        return goalValue;
+    }
+
+    public int getMilestoneFreq() {
+        return milestoneFreq;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public boolean isSync() {
+        return sync;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setActivityType(int activityType) {
+        this.activityType = activityType;
+    }
+
+    public void setGoalType(int goalType) {
+        this.goalType = goalType;
+    }
+
+    public void setGoalValue(int goalValue) {
+        this.goalValue = goalValue;
+    }
+
+    public void setMilestoneFreq(int milestoneFreq) {
+        this.milestoneFreq = milestoneFreq;
+    }
+
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setSync(boolean sync) {
+        this.sync = sync;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+}
--- app/src/main/java/ducere/lechal/pod/sqlite/TablesColumns.java
+++ app/src/main/java/ducere/lechal/pod/sqlite/TablesColumns.java
@@ -9,6 +9,7 @@ public class TablesColumns {
     public TablesColumns() {
     }
 
+    private static final String CREATE_TABLE = "CREATE TABLE ";
     private static final String PRIMARY_KEY = " PRIMARY KEY";
     private static final String TEXT_TYPE = " TEXT";
     private static final String INT_TYPE = " INTEGER";
@@ -34,8 +35,25 @@ public class TablesColumns {
         public static final String COLUMN_NAME_UPDATED = "updated";
     }
 
+    public static abstract class SessionEntry implements BaseColumns {
+        public static final String TABLE_NAME = "Session";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_ACTIVITY_TYPE = "activityType";
+        public static final String COLUMN_NAME_GOAL_TYPE = "goalType";
+        public static final String COLUMN_NAME_GOAL_VALUE = "goalValue";
+        public static final String COLUMN_NAME_MILESTONE_FREQ = "milestoneFreq";
+        public static final String COLUMN_NAME_STEPS = "steps";
+        public static final String COLUMN_NAME_CALORIES = "calories";
+        public static final String COLUMN_NAME_TIME = "time";
+        public static final String COLUMN_NAME_DISTANCE = "distance";
+        public static final String COLUMN_NAME_STATUS = "status";
+        public static final String COLUMN_NAME_SYNC = "sync";
+        public static final String COLUMN_NAME_START_TIME = "startTime";
+        public static final String COLUMN_NAME_END_TIME = "endTime";
+    }
+
     static final String SQL_CREATE_TAGS =
-            "CREATE TABLE " + TagEntry.TABLE_NAME + " (" +
+            CREATE_TABLE + TagEntry.TABLE_NAME + " (" +
                     TagEntry.COLUMN_NAME_PLACE_ID + TEXT_TYPE + PRIMARY_KEY + COMMA_SEP +
                     TagEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
                     TagEntry.COLUMN_NAME_ADDRESS + TEXT_TYPE + COMMA_SEP +
@@ -49,10 +67,29 @@ public class TablesColumns {
             "DROP TABLE IF EXISTS " + TagEntry.TABLE_NAME;
 
     static final String SQL_CREATE_FITNESS =
-            "CREATE TABLE " + FitnessEntry.TABLE_NAME + " (" +
+            CREATE_TABLE + FitnessEntry.TABLE_NAME + " (" +
                     FitnessEntry.COLUMN_NAME_FITNESS_ID + BIGINT_TYPE + PRIMARY_KEY + COMMA_SEP +
                     FitnessEntry.COLUMN_NAME_JSON + TEXT_TYPE + COMMA_SEP +
                     FitnessEntry.COLUMN_NAME_UPDATED + BIGINT_TYPE +
                     " )";
+
+    static final String SQL_CREATE_SESSIONS =
+            CREATE_TABLE + SessionEntry.TABLE_NAME + " (" +
+                    SessionEntry._ID + INT_TYPE + PRIMARY_KEY + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_NAME + TEXT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_ACTIVITY_TYPE + INT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_GOAL_TYPE + INT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_GOAL_VALUE + INT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_MILESTONE_FREQ + INT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_STEPS + INT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_CALORIES + INT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_TIME + INT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_DISTANCE + INT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_START_TIME + BIGINT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_END_TIME + BIGINT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_STATUS + INT_TYPE + COMMA_SEP +
+                    SessionEntry.COLUMN_NAME_SYNC + INT_TYPE +
+                    " )";
+
 
 }
